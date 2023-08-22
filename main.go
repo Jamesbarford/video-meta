e52e138f49002d224dfb99ee363bebe942255b81
Jamesbarford <jamesbarfordevans@gmail.com>
2023-08-22 15:10:52 +0100

    more boilerplate

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-    "fmt"
+    "Jamesbarford/video-meta/server"
 )
 
-
 func main() {
-	fmt.Println("Hello world")
+    ServerMain()
 }
--- server/server.go
+++ server/server.go
@@ -1,17 +1,15 @@
-package src
+package server 
 
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
-func ServerMain(port string) {
+func ServerMain() {
     e := echo.New()
     e.GET(".", func (c echo.Context) error {
         return c.String(http.StatusOK, "HelloWorld!")
     })
-
-    e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
+    e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", "8080")))
 }
